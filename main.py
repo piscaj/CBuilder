583dcbd9c46b253816c260bc914c7eb7f5163db7
piscaj <piscaj>
2020-06-15 00:30:42 -0400

    Add table

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-import kivy
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
 
 class CBuilderApp(MDApp):
-
-    def build(self):
+	
+	data = {
+		'code-braces': 'New',
+		'send': 'Send',
+		'settings': 'Settings',
+      	}
+     
+	column = [
+		('#', 'dp(30)'),
+		('Name','dp(30)'),
+		]
+      	
+	row = [
+		('1','2'),
+      	]
+      	
+	def build(self):#
         #self.theme_cls.primary_palette = "Red"
-        self.theme_cls.theme_style = "Dark"
-        return Builder.load_file("main.kv")
+		self.theme_cls.theme_style = "Dark"
+		return Builder.load_file("main.kv")
 
 if __name__ == "__main__":
-    CBuilderApp().run()
+    CBuilderApp().run
