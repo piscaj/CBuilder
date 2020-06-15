8d8e34a8be07ae0f62e376cd5c65aa85c248d57b
Jason Piscatelli <JasonPiscatelli@Jason-Sr.localdomain>
2020-06-15 09:23:18 -0400

    Replace table with list

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import kivy
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineAvatarIconListItem, IconRightWidget, IRightBodyTouch
+from kivymd.uix.selectioncontrol import MDCheckbox
+from kivymd.icon_definitions import md_icons
+
+
+
+class ListItemWithCheckbox(OneLineAvatarIconListItem):
+    '''Custom list item.'''
+
+    icon = StringProperty("android")
+
+class RightCheckbox(IRightBodyTouch, MDCheckbox):
+    '''Custom right container.'''
+
+class ListItemWithCheckbox(OneLineAvatarIconListItem):
+    '''Custom list item.'''
+
+    icon = StringProperty("android")
 
 class CBuilderApp(MDApp):
     
@@ -15,6 +35,13 @@ class CBuilderApp(MDApp):
         #self.theme_cls.primary_palette = "Red"
         self.theme_cls.theme_style = "Dark"
         return Builder.load_file("main.kv")
+    
+    def on_start(self):
+        icons = list(md_icons.keys())
+        for i in range(30):
+            self.root.ids.scroll.add_widget(
+                ListItemWithCheckbox(text=f"Item {i}", icon=icons[i])
+            )
 
 if __name__ == "__main__":
     CBuilderApp().run()
