0f2ae8b62d74a9026a19f5ec3b55ea4f09344a57
Jason Piscatelli <JasonPiscatelli@Jason-Sr.localdomain>
2020-06-14 20:44:46 -0400

    Add main

---
+++ main.py
@@ -0,0 +1,16 @@
+import kivy
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivymd.theming import ThemableBehavior
+
+
+class CBuilderApp(MDApp):
+
+    def build(self):
+        #self.theme_cls.primary_palette = "Red"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_file("main.kv")
+
+
+if __name__ == "__main__":
+    CBuilderApp().run()
